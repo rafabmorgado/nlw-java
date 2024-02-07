3ce70271d2c440c9e4e3d50d57187b6b43547f9f
Rafa Morgado <93506265+rafabmorgado@users.noreply.github.com>
2024-02-06 23:19:49 -0300

    feat: add maping entities to database

--- src/main/java/dev/morgadorafa/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/dev/morgadorafa/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -1,8 +1,21 @@
 package dev.morgadorafa.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +23,28 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "certifications")
 public class CertificationStudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @JoinColumn(name="student_id")
     private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
     private int grate;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
     List<AnswersCertificationsEntity> answersCertificationsEntities;
+
 
 }
--- src/main/java/dev/morgadorafa/certification_nlw/modules/students/entities/StudentEntity.java
+++ src/main/java/dev/morgadorafa/certification_nlw/modules/students/entities/StudentEntity.java
@@ -1,8 +1,18 @@
 package dev.morgadorafa.certification_nlw.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,11 +21,23 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name="students")
 public class StudentEntity {
     
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String email;
-    List<CertificationStudentEntity> certificationStudentEntity;
+
+    @OneToMany(mappedBy = "studentEntity")
+    private List<CertificationStudentEntity> certificationStudentEntity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
