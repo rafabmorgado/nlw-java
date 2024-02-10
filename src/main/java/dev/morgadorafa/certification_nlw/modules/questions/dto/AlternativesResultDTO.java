0cb5ae1dc7808c4dfbe637e299e05ebbc5d1ff92
Rafa Morgado <93506265+rafabmorgado@users.noreply.github.com>
2024-02-09 23:08:51 -0300

    feat: finish questions module

---
+++ src/main/java/dev/morgadorafa/certification_nlw/modules/questions/dto/AlternativesResultDTO.java
@@ -0,0 +1,18 @@
+package dev.morgadorafa.certification_nlw.modules.questions.dto;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AlternativesResultDTO {
+    
+    private UUID id;
+    private String description;
+}
---
+++ src/main/java/dev/morgadorafa/certification_nlw/modules/questions/repositories/QuestionRepository.java
@@ -0,0 +1,11 @@
+package dev.morgadorafa.certification_nlw.modules.questions.repositories;
+
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import dev.morgadorafa.certification_nlw.modules.questions.entities.QuestionEntity;
+
+public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID>{
+    
+    List<QuestionEntity> findByTechnology(String technology);
+}
